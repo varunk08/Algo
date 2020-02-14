d48bcedb71a779841a24bc6824d893f5d79eab67
vkaruppa <varunkumar.karuppannan@amd.com>
2020-02-14 10:40:10 -0500

    fixed bug at first index overflow

--- increment_int.cpp
+++ increment_int.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,6 +10,17 @@ void increment_int(vector<int>* pVec) {
   int carry = 1;
   int i = size(A) - 1;
   while (carry) {
+    if (i < 0) {
+        vector<int> temp(size(A));
+        temp.assign(A.cbegin(), A.cend());
+        A.resize(size(temp) + 1);
+        A[0] = carry;
+        for (int i = 0; i < size(temp); i++) {
+            A[i + 1] = temp[i];
+        }
+        break;
+    }
+
     int new_val = A[i] + carry;
     carry = new_val / 10;
     A[i] = new_val % 10;
@@ -18,7 +30,7 @@ void increment_int(vector<int>* pVec) {
 }
 int main()
 {
-  vector<int> input_num = { 1, 9, 9}; // result= 124
+  vector<int> input_num = { 9, 9, 9, 9}; // result= 124
   increment_int(&input_num);
   for (auto i : input_num) {
     cout << i << " ";
