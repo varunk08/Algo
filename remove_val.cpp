e138fb4d1c4e6a558e569aec72226fc4325e1953
Karuppannan <vkaruppa@amd.com>
2020-02-21 17:50:50 -0500

    remove val. incomplete

---
+++ remove_val.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  vector<int> nums = {4, 4, 4, 5 } ;// { 1, 2, 3, 4, 4, 5, 5, 6, 7};
+  int val = 4;
+
+  int garb = size(nums) - 1;
+
+  int i = 0;
+  for (i = 0; i < size(nums); i++) {
+    if (nums[i] == val) {
+      while (nums[garb] == val && garb > i) {
+        garb--;
+      }
+
+      if (garb <= i) {
+        break;
+      }
+      
+      swap(nums[i], nums[garb--]);
+    }
+  }
+
+  cout << "length = " << garb + 1 << endl;
+  return 0;
+}
